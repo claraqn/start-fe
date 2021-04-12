786fb8e8bb88dedb512cb6c7c2b37f9a81785c8a
unknown <claraqn@naver.com>
2021-04-13 00:49:54 +0900

    7주차수업

---
+++ 07/test.js
@@ -0,0 +1,18 @@
+const a = 3;
+const b = 4;
+console.log(b);
+const c = 5;
+
+// const a = {
+//   hello: 1,
+//   what: 4,
+//   d: 1,
+//   F: 3,
+//   d: 7,
+//   d,
+// };
+
+// root 디렉토리에 .prettiersrc 만들어서
+// 설정을 적용시켜줘야함
+// 개인이 아닌 팀 개발을 할 경우
+// vscode 설정을 일일이 다 바꿔줄 수 없기 때문
