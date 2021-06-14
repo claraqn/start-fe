8b9e26c0d6884518d422d86aec3a5999e042907a
unknown <claraqn@naver.com>
2021-06-14 16:47:30 +0900

    test

--- 11/src/form-input.js
+++ 11/src/form-input.js
@@ -18,9 +18,10 @@ function init(data) {
     //   isDone: false,
     // }));
     data.push({
-      ...todoModel,
-      text,
-      isDone: false,
+      // startfe 의 향상된 객체 리터럴 파트
+      ...todoModel, // todoModel에 있는 값을 복사해서 가져오고, 필요한 값(text, isDone)만 덮어씌우기
+      text, // text(todoModel의 text) : text(init 함수의 text)
+      isDone: false, // isDone(todoModel의 isDone) : isDone
     });
 
     render(data);
@@ -29,4 +30,6 @@ function init(data) {
 
 export default {
   init,
+  // init(data) 이렇게 x
+  // 매개변수 적어주지 x
 };
